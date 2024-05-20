04882a98dc353b45b1977ab3b7b235b1078e426d
b354b <112465395+b354b@users.noreply.github.com>
2024-05-20 15:01:53 +0900

    Delete blog01 directory

--- blog01/src/main/java/b354b/blog01/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package b354b.blog01.springbootdeveloper.repository;
-
-import b354b.blog01.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
